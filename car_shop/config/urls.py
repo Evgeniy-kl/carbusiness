057847de1ca2862eb1db03ad3b60470e6777ec2b
Evgeniy-kl <z_klimovich@bk.ru>
2022-02-11 13:47:18 +0300

    added user registration

--- car_shop/config/urls.py
+++ car_shop/config/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('car.urls')),
     #path('', include('showroom.urls')),
-    #path('', include('customer.urls')),
+    path('', include('customer.urls')),
     #path('', include('supplier.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
--- car_shop/customer/serializers.py
+++ car_shop/customer/serializers.py
@@ -1,2 +1,9 @@
 from rest_framework import serializers
+from customer.models import User, Customer, Offer
+
+
+class CreateUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username', 'password']
 
