cc28fd6b77e35db5f2d619f3aff589315c0330ee
Evgeniy-kl <z_klimovich@bk.ru>
2022-02-14 14:41:41 +0300

    changed model admin

--- car_shop/supplier/admin.py
+++ car_shop/supplier/admin.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 from supplier.models import Supplier, SupplierHistory, DiscountSupplier
 
 
-@admin.register(Supplier, SupplierHistory, DiscountSupplier)
+@admin.register(Supplier)
 class DefaultAdmin(admin.ModelAdmin):
-    pass
+    list_filter = ('foundation', 'showrooms', )
+
+
+@admin.register(SupplierHistory, DiscountSupplier)
+class DefaultAdmin(admin.ModelAdmin):
+    list_filter = ('supplier',)
